3f0814e2dfb836b02219534dc8648ff6cf3e7dc1
mehulbasu <mehulbasu@gmail.com>
2022-02-18 00:34:33 -0600

    app icon

---
+++ Q App/Shared/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Patron on 2/15/22.
+//
+
+import SwiftUI
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews:some View {
+        ContentView()
+    }
+}
---
+++ Q App/Shared/Q_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Q_AppApp.swift
+//  Shared
+//
+//  Created by Patron on 2/15/22.
+//
+
+import SwiftUI
+
+@main
+struct Q_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
